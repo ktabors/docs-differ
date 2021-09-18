65a63aa3b428a3be3cd12b2c6f35c5999435eccd
Kyle Taborski <ktabors@yahoo.com>
2021-09-17 21:14:47 -0700

    proof of concept

--- src/index.js
+++ src/index.js
@@ -1,25 +1,46 @@
 var http = require("http");
 const Pageres = require("pageres");
+const diffScreenshots = require("screenshots-diff").default;
+var fs = require("fs");
+
+console.log('diffScreenshots', diffScreenshots);
+
+var oldWebsite = __dirname + '/old';
+var newWebsite = __dirname + '/new';
+var diff = __dirname + '/diff';
+
+if (!fs.existsSync(oldWebsite)){
+    fs.mkdirSync(oldWebsite);
+}
+if (!fs.existsSync(newWebsite)){
+    fs.mkdirSync(newWebsite);
+}
+if (!fs.existsSync(diff)){
+    fs.mkdirSync(diff);
+}
+
 
 (async () => {
-  await new Pageres({ delay: 2 })
-    .src(
-      "https://github.com/adobe/react-spectrum",
-      ["480x320", "1024x768", "iphone 5s"],
-      { crop: true }
-    )
-    .src("https://https://react-spectrum.adobe.com", ["1280x1024", "1920x1080"])
-    .src("data:text/html,<h1>Awesome!</h1>", ["1024x768"])
-    .dest(__dirname)
+  let oldImageBuffer = await new Pageres({ delay: 2, filename: 'apple' })
+    .src("https://react-spectrum.adobe.com", ["1280x3024"])
+    .dest(oldWebsite)
     .run();
 
-  console.log("Finished generating screenshots!");
-})();
+  let newImageBuffer = await new Pageres({ delay: 2, filename: 'apple' })
+    .src("https://reactspectrum.blob.core.windows.net/reactspectrum/0d525b64b5f85f375c5477ee78a7287ff9b264f4/docs/architecture.html", ["1280x3024"])
+    .dest(newWebsite)
+    .run();
 
-//create a server object:
-http
-  .createServer(function (req, res) {
-    res.write("Hello Candidate!!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+  console.log('__dirname', __dirname);
+  console.log("Finished generating screenshots!", oldImageBuffer, newImageBuffer);
+
+  await diffScreenshots(oldWebsite, newWebsite, diff, 0.03)
+    .then(result => {
+      if (result) {
+        console.log("diff results", JSON.stringify(result, null, 1));
+      }
+    })
+    .catch(err => {
+      logError(`** ERROR ** ${err}`);
+    });
+})();
