2bb71eb01eaa678651fafd10e1d59b5b7ce05244
Kyle Taborski <ktabors@yahoo.com>
2021-10-06 17:31:29 -0700

    switched to using puppeteer for screenshots and it also does the site crawling

--- src/index.js
+++ src/index.js
@@ -1,46 +1,145 @@
-var http = require("http");
-const Pageres = require("pageres");
-const diffScreenshots = require("screenshots-diff").default;
-var fs = require("fs");
+const diffScreenshots = require('screenshots-diff').default;
+const mkdirp = require('mkdirp');
+const path = require('path');
+const puppeteer = require('puppeteer');
+const rimraf = require('rimraf');
+const URL = require('url').URL;
 
-console.log('diffScreenshots', diffScreenshots);
+const puppeteeriPhone11 = puppeteer.devices['iPhone 11'];
 
-var oldWebsite = __dirname + '/old';
-var newWebsite = __dirname + '/new';
-var diff = __dirname + '/diff';
+const baselineDir = 'dist/baseline';
+const currentDir = 'dist/current';
+const diffDir = 'dist/diff';
 
-if (!fs.existsSync(oldWebsite)){
-    fs.mkdirSync(oldWebsite);
-}
-if (!fs.existsSync(newWebsite)){
-    fs.mkdirSync(newWebsite);
-}
-if (!fs.existsSync(diff)){
-    fs.mkdirSync(diff);
-}
+const baselineCommandParam = '-b';
+const currentCommandParam = '-c';
 
+rimraf.sync(baselineDir);
+rimraf.sync(currentDir);
+rimraf.sync(diffDir);
+
+const visitedBaseline = {};
+const visitedCurrent = {};
+
+var limitWalk = 0;
 
 (async () => {
-  let oldImageBuffer = await new Pageres({ delay: 2, filename: 'apple' })
-    .src("https://react-spectrum.adobe.com", ["1280x3024"])
-    .dest(oldWebsite)
-    .run();
+  let exitCode = 0;
+  console.time('executionTime');
 
-  let newImageBuffer = await new Pageres({ delay: 2, filename: 'apple' })
-    .src("https://reactspectrum.blob.core.windows.net/reactspectrum/0d525b64b5f85f375c5477ee78a7287ff9b264f4/docs/architecture.html", ["1280x3024"])
-    .dest(newWebsite)
-    .run();
+  let urls = getUrls();
+  if (urls.length !== 2) {
+    console.log('Usage: \'node src/index.js -b <baseline site url> -c <current site to diff against baseline url>\'');
+    return 1;
+  }
 
-  console.log('__dirname', __dirname);
-  console.log("Finished generating screenshots!", oldImageBuffer, newImageBuffer);
+  try {
+    await puppetUrl(urls[0], visitedBaseline, baselineDir);
+    limitWalk = 0;
+    await puppetUrl(urls[1], visitedCurrent, currentDir);
 
-  await diffScreenshots(oldWebsite, newWebsite, diff, 0.03)
+    let diffResult = await diffSites();
+  } catch (e) {
+    exitCode = 1;
+    throw e;
+  }
+
+  console.timeEnd('executionTime');
+  return exitCode;
+})();
+
+function getUrls() {
+  var myArgs = process.argv.slice(2);
+
+  let urls = [];
+  if (myArgs.includes(baselineCommandParam)) {
+    urls.push(myArgs[myArgs.indexOf(baselineCommandParam) + 1]);
+  }
+  if (myArgs.includes(currentCommandParam)) {
+    urls.push(myArgs[myArgs.indexOf(currentCommandParam) + 1]);
+  }
+
+  return urls;
+}
+
+async function diffSites() {
+  let jsonResult = {};
+  await diffScreenshots(baselineDir, currentDir, diffDir, 0.03)
     .then(result => {
       if (result) {
+        jsonResult = result;
         console.log("diff results", JSON.stringify(result, null, 1));
       }
     })
     .catch(err => {
-      logError(`** ERROR ** ${err}`);
+      console.error(`** ERROR ** ${err}`);
     });
-})();
+
+  return jsonResult;
+}
+
+async function puppetUrl(url, visited, storageDirectory) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  let lastIndex = new URL(url).pathname.lastIndexOf('/');
+  await walk(page, url, new URL(url).pathname.substring(0, lastIndex), visited, storageDirectory);
+
+  await browser.close();
+}
+
+async function walk(page, href, rootPath, visited, storageDirectory) {
+  // this is to limit the runtime for dev
+  limitWalk++;
+  if (limitWalk >= 10) {
+    return;
+  }
+  // end dev code block
+  let url = new URL(href, page.url());
+  url.hash = '';
+  if (visited[url.pathname]) {
+    return;
+  }
+
+  let filename = url.pathname.replace(rootPath, '').slice(1).split('.')[0].replace('/', '~~');
+  visited[url.pathname] = [`${filename}_desktop.png`, `${filename}_mobile.png`];
+
+  try {
+    await page.goto(url.toString());
+    await screenshot(page, filename, rootPath, storageDirectory);
+  } catch (e) {
+    console.log('error with screenshot: ', url.toString());
+  }
+
+  let hrefs = await page.$$eval('a[href]', as => as.map(a => a.href));
+  console.log('hrefs', hrefs);
+
+  for (let href of hrefs) {
+    let u = new URL(href, page.url());
+    if (u.host === url.host) {
+      await walk(page, href, rootPath, visited, storageDirectory);
+    }
+  }
+}
+
+async function screenshot(page, filename, rootPath, storageDirectory) {
+  mkdirp.sync(storageDirectory);
+
+  await page.setViewport({
+    width: 1366,
+    height: 784
+  });
+  await page.waitForTimeout(100);
+
+  await page.screenshot({
+    path: `${storageDirectory}/${filename}_desktop.png`,
+    fullPage: true
+  });
+
+  await page.emulate(puppeteeriPhone11);
+
+  await page.screenshot({
+    path: `${storageDirectory}/${filename}_mobile.png`,
+    fullPage: true
+  });
+}
