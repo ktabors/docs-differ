270e188c51f5ef1f99e67416a8e49fde7ef5b61b
Kyle Taborski <ktabors@yahoo.com>
2021-10-06 17:51:58 -0700

    adding comments to explain code

--- src/index.js
+++ src/index.js
@@ -7,22 +7,31 @@ const URL = require('url').URL;
 
 const puppeteeriPhone11 = puppeteer.devices['iPhone 11'];
 
+// setting up the directories
 const baselineDir = 'dist/baseline';
 const currentDir = 'dist/current';
 const diffDir = 'dist/diff';
-
-const baselineCommandParam = '-b';
-const currentCommandParam = '-c';
 
 rimraf.sync(baselineDir);
 rimraf.sync(currentDir);
 rimraf.sync(diffDir);
 
+// command line options
+const baselineCommandParam = '-b';
+const currentCommandParam = '-c';
+
 const visitedBaseline = {};
 const visitedCurrent = {};
 
+// dev prop for debugging with quick execution
 var limitWalk = 0;
 
+/**
+ * Main execution loop:
+ *   - gets the two URLs to diff
+ *   - crawls and screenshots them
+ *   - and runs the diff.
+ */
 (async () => {
   let exitCode = 0;
   console.time('executionTime');
@@ -48,6 +57,9 @@ var limitWalk = 0;
   return exitCode;
 })();
 
+/**
+ * Gets the command line options which is the URLs to compare.
+ */
 function getUrls() {
   var myArgs = process.argv.slice(2);
 
@@ -62,6 +74,10 @@ function getUrls() {
   return urls;
 }
 
+/**
+ * Calls the screenshot diff tool on the two directories and creates a json
+ * report that is saved and logged.
+ */
 async function diffSites() {
   let jsonResult = {};
   await diffScreenshots(baselineDir, currentDir, diffDir, 0.03)
@@ -78,6 +94,9 @@ async function diffSites() {
   return jsonResult;
 }
 
+/**
+ * Sets up puppeteer and call walk which recursively walks the site.
+ */
 async function puppetUrl(url, visited, storageDirectory) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
@@ -88,6 +107,12 @@ async function puppetUrl(url, visited, storageDirectory) {
   await browser.close();
 }
 
+/**
+ * Checks is a URL has been visited, if it hasn't it visits it and takes a
+ * screenshot. Visiting also means getting all of its links and calling this
+ * function, walk, on all the links that are within this site. It ignores
+ * external links.
+ */
 async function walk(page, href, rootPath, visited, storageDirectory) {
   // this is to limit the runtime for dev
   limitWalk++;
@@ -101,18 +126,23 @@ async function walk(page, href, rootPath, visited, storageDirectory) {
     return;
   }
 
+  /* two things
+   * - for the CI builds we have extra pathname, this removes that
+   * - the screenshot tool doesn't handle subdirectories so we include those in
+   *   the file names with ~~ indicating where a directory slash was
+   */
   let filename = url.pathname.replace(rootPath, '').slice(1).split('.')[0].replace('/', '~~');
   visited[url.pathname] = [`${filename}_desktop.png`, `${filename}_mobile.png`];
 
   try {
     await page.goto(url.toString());
-    await screenshot(page, filename, rootPath, storageDirectory);
+    await screenshot(page, filename, storageDirectory);
   } catch (e) {
     console.log('error with screenshot: ', url.toString());
   }
 
   let hrefs = await page.$$eval('a[href]', as => as.map(a => a.href));
-  console.log('hrefs', hrefs);
+  // console.log('hrefs', hrefs);
 
   for (let href of hrefs) {
     let u = new URL(href, page.url());
@@ -122,13 +152,18 @@ async function walk(page, href, rootPath, visited, storageDirectory) {
   }
 }
 
-async function screenshot(page, filename, rootPath, storageDirectory) {
+/**
+ * Talks a full page screenshot of the current most common browser viewport and
+ * a simulated iPhone 11 for the mobile rendering.
+ */
+async function screenshot(page, filename, storageDirectory) {
   mkdirp.sync(storageDirectory);
 
   await page.setViewport({
     width: 1366,
     height: 784
   });
+  // this seems to handle screenshot issues, might need to increase as we use this
   await page.waitForTimeout(100);
 
   await page.screenshot({
