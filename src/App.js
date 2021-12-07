d6f410cc6c76844060314c852593add01d119482
Daniel Lu <dl1644@gmail.com>
2021-12-07 15:20:51 -0800

    Updating layout
    Making the screenshot wrapping div take what available height there is

--- src/App.js
+++ src/App.js
@@ -24,44 +24,38 @@ function App() {
 
   return (
     <Provider theme={defaultTheme}>
-      <View height="100vh" overflow="scroll">
-        <Flex direction="column" width="80%" minHeight="100%" margin="auto">
-          <Text marginTop="size-400" UNSAFE_style={{fontWeight: 'bold'}}>Summary: {summary}</Text>
+      <Flex direction="column" width="80%" height="100vh" margin="auto">
+        <Text marginTop="size-400" UNSAFE_style={{fontWeight: 'bold'}}>Summary: {summary}</Text>
+        <Slider
+          marginTop="size-100"
+          minValue={1}
+          maxValue={numImages}
+          value={position + 1}
+          onChange={(index) => setPosition(index - 1)}
+          label="Nth image diff displayed"
+          getValueLabel={(index) => `${index} of ${numImages} changes`}
+          width="50%" />
+        <Flex direction="column" gap="size-200" marginTop="size-600" marginBottom="size-300" minHeight="0px" flex={1}>
+          <Text UNSAFE_style={{fontWeight: 'bold'}}>File: {imgPath}</Text>
           <Slider
-            marginTop="size-100"
+            label="Zoom"
+            value={zoom}
+            onChange={setZoom}
             minValue={1}
-            maxValue={numImages}
-            value={position + 1}
-            onChange={(index) => setPosition(index - 1)}
-            label="Nth image diff displayed"
-            getValueLabel={(index) => `${index} of ${numImages} changes`}
-            width="50%" />
-          <Flex direction="column" gap="size-200" marginTop="size-600">
-            <Text UNSAFE_style={{fontWeight: 'bold'}}>File: {imgPath}</Text>
-            <Flex direction="column">
-              <Slider
-                label="Zoom"
-                value={zoom}
-                onChange={setZoom}
-                minValue={1}
-                maxValue={4} />
-              <div style={{overflow: 'auto'}} tabIndex={0}>
-                <img
-                  alt={`image diff, file name: ${imgPath}`}
-                  src={imgPath}
-                  style={{
-                    display: 'block',
-                    height: '100%',
-                    width: '100%',
-                    maxWidth: '100%',
-                    transform: `scale(${zoom})`,
-                    transformOrigin: '0 0'
-                  }} />
-              </div>
-            </Flex>
-          </Flex>
+            maxValue={4} />
+          <div style={{overflow: 'auto', minHeight: "0px", flex: 1}} tabIndex={0}>
+            <img
+              alt={`image diff, file name: ${imgPath}`}
+              src={imgPath}
+              style={{
+                display: 'block',
+                maxWidth: '100%',
+                transform: `scale(${zoom})`,
+                transformOrigin: '0 0'
+              }} />
+          </div>
         </Flex>
-      </View>
+      </Flex>
     </Provider>
   );
 }
