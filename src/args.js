f38c96d5b26ca4cc82478ac5a8ac4ca99ed43dcd
Kyle Taborski <ktabors@yahoo.com>
2021-12-06 19:45:38 -0800

    Refactor breaking the code into multiple files (#2)
    * moved cli arg handling and screen diff code to their own files
    
    * moving crawling code to its own file
    
    * bumping version number

---
+++ src/args.js
@@ -0,0 +1,96 @@
+// command line options
+const baselineCommandParam = '-b';
+const currentCommandParam = '-c';
+const screenshotMaxCommandParam = '-s';
+const disableMobileCommandParam = '-m';
+const disableDesktopCommandParam = '-d';
+const clusterSizeCommandParam = '-k';
+const quietLoggingCommandParam = '-q';
+
+const defaultValues = {
+  screenshotLimit: -1,
+  disableDesktopScreenshots: false,
+  disableMobileScreenshots: false,
+  clusterMaxConcurrency: 10,
+  verboseLogMessages: true
+};
+
+/**
+ * Gets the urls to compare from the command line args.
+ */
+function getUrls() {
+  let myArgs = process.argv.slice(2);
+
+  let urls = [];
+  if (myArgs.includes(baselineCommandParam)) {
+    urls.push(myArgs[myArgs.indexOf(baselineCommandParam) + 1]);
+  }
+  if (myArgs.includes(currentCommandParam)) {
+    urls.push(myArgs[myArgs.indexOf(currentCommandParam) + 1]);
+  }
+
+  return urls;
+}
+
+// used in two places
+function logUsage() {
+  console.log(`Usage: 'node src/index.js ${baselineCommandParam} <baseline site url> ${currentCommandParam} <current site to diff against baseline url>'`);
+  console.log(`Other options include ${disableMobileCommandParam}, ${disableDesktopCommandParam} and ${screenshotMaxCommandParam} <integer>`);
+  console.log(`  ${disableMobileCommandParam} disable mobile screenshots, default false`);
+  console.log(`  ${disableDesktopCommandParam} disable desktop screenshots, default false`);
+  console.log(`  ${screenshotMaxCommandParam} limit screenshots taken for all possible to this number`);
+  console.log(`  ${clusterSizeCommandParam} max cluster size for concurrency, default 10`);
+  console.log(`  ${quietLoggingCommandParam} quiets some log messages`);
+}
+
+/**
+ * Set the command line args:
+ *   - disable mobile screenshots
+ *   - disable desktop screenshots
+ *   - max number of screenshots
+ */
+function processArgs() {
+  let myArgs = process.argv.slice(2);
+  let {
+    screenshotLimit,
+    clusterMaxConcurrency,
+    disableMobileScreenshots,
+    disableDesktopScreenshots,
+    verboseLogMessages
+  } = defaultValues;
+
+  if (myArgs.includes(screenshotMaxCommandParam)) {
+    screenshotLimit = parseInt(myArgs[myArgs.indexOf(screenshotMaxCommandParam) + 1], 10);
+    if (isNaN(screenshotLimit)) {
+      logUsage();
+      process.exit(1);
+    } else if (screenshotLimit <= 0) {
+      screenshotLimit = -1;
+    }
+  }
+  if (myArgs.includes(clusterSizeCommandParam)) {
+    clusterMaxConcurrency = parseInt(myArgs[myArgs.indexOf(clusterSizeCommandParam) + 1], 10);
+    if (isNaN(clusterMaxConcurrency)) {
+      logUsage();
+      process.exit(1);
+    } else if (clusterMaxConcurrency <= 0) {
+      clusterMaxConcurrency = 10;
+    }
+  }
+  if (myArgs.includes(disableMobileCommandParam)) {
+    disableMobileScreenshots = true;
+  }
+  if (myArgs.includes(disableDesktopCommandParam)) {
+    disableDesktopScreenshots = true;
+  }
+  if (myArgs.includes(quietLoggingCommandParam)) {
+    verboseLogMessages = false;
+  }
+
+  return {screenshotLimit, clusterMaxConcurrency, disableMobileScreenshots, disableDesktopScreenshots, verboseLogMessages};
+}
+
+exports.getUrls = getUrls;
+exports.logUsage = logUsage;
+exports.processArgs = processArgs;
+exports.defaultParamValues = defaultValues;
