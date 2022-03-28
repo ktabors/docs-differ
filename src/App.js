de00213e8fd93e699d0a755049d023f370499b04
Kyle Taborski <ktabors@yahoo.com>
2022-03-28 16:15:37 -0700

    adding tabs and carview to review diff images

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import {
   defaultTheme,
   Flex,
+  Item,
   Provider,
-  Slider,
-  Text,
-  View
+  TabList,
+  TabPanels,
+  Tabs,
+  Text
 } from '@adobe/react-spectrum';
+import {DiffCardView} from './DiffCardView';
+import {DiffSlider} from './DiffSlider';
 import diffJSON from './docs-differ/diff/diff.json';
 import React, {useState} from 'react';
 
-function importAll(r) {
-  return r.keys().map(r);
-}
-
-const images = importAll(require.context('./docs-differ/diff', false, /\.(png)$/));
 function App() {
-  let [position, setPosition] = useState(0);
-  let [zoom, setZoom] = useState(1);
-  let numImages = images.length;
-  let imgPath = images[position].default;
   let summary = diffJSON.message;
   summary = summary?.replace(/Alright, there was | But are they regressions or expected changes \?/g,'')
 
@@ -26,35 +21,20 @@ function App() {
     <Provider theme={defaultTheme}>
       <Flex direction="column" width="80%" height="100vh" margin="auto">
         <Text marginTop="size-400" UNSAFE_style={{fontWeight: 'bold'}}>Summary: {summary}</Text>
-        <Slider
-          marginTop="size-100"
-          minValue={1}
-          maxValue={numImages}
-          value={position + 1}
-          onChange={(index) => setPosition(index - 1)}
-          label="Nth image diff displayed"
-          getValueLabel={(index) => `${index} of ${numImages} changes`}
-          width="50%" />
-        <Flex direction="column" gap="size-200" marginTop="size-600" marginBottom="size-300" minHeight="0px" flex={1}>
-          <Text UNSAFE_style={{fontWeight: 'bold'}}>File: {imgPath}</Text>
-          <Slider
-            label="Zoom"
-            value={zoom}
-            onChange={setZoom}
-            minValue={1}
-            maxValue={4} />
-          <div style={{overflow: 'auto', minHeight: "0px", flex: 1}} tabIndex={0}>
-            <img
-              alt={`image diff, file name: ${imgPath}`}
-              src={imgPath}
-              style={{
-                display: 'block',
-                maxWidth: '100%',
-                transform: `scale(${zoom})`,
-                transformOrigin: '0 0'
-              }} />
-          </div>
-        </Flex>
+        <Tabs aria-label="Review Diffs">
+          <TabList>
+            <Item key="cardview">Card</Item>
+            <Item key="slider">Slider</Item>
+          </TabList>
+          <TabPanels>
+            <Item key="cardview">
+              <DiffCardView />
+            </Item>
+            <Item key="slider">
+              <DiffSlider />
+            </Item>
+          </TabPanels>
+        </Tabs>
       </Flex>
     </Provider>
   );
